21128eb96268ce7400bbe86e14bd4be2901ddba1
eric beasley <ejbeasley@gmail.com>
2015-11-20 19:32:52 -0800

    Correct NumberHander implrementation

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -10,19 +10,40 @@
 
 @implementation NumberHandler
 
-- (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+- (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number
+{
+    return [NSNumber numberWithDouble:[number doubleValue]*2];
 }
 
-- (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+- (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber
+{
+    NSNumber *number1;
+    NSNumber *number2;
+    
+    if (otherNumber > number) {
+        number1 = [NSNumber numberWithInteger:number];
+        number2 = [NSNumber numberWithInteger:otherNumber];
+    } else {
+        number1 = [NSNumber numberWithInteger:otherNumber];
+        number2 = [NSNumber numberWithInteger:number];
+    }
+    
+    NSMutableArray *returnArray = [[NSMutableArray alloc] init];
+    for (NSInteger i = number1.integerValue; i <= number2.integerValue; i++) {
+        [returnArray addObject:[NSNumber numberWithInteger:i]];
+    }
+    return [returnArray copy];
 }
 
-- (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+- (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers
+{
+    NSNumber *lowestNumber = arrayOfNumbers[0];
+    for (NSNumber *number in arrayOfNumbers) {
+        if (number.integerValue < lowestNumber.integerValue) {
+            lowestNumber = number;
+        }
+    }
+    return lowestNumber.integerValue;
 }
 
 @end
